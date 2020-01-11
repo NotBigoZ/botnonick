84bac1ec64d341e240b8ba4bfdbc480ccfd0faff
NotBigoZ <59773657+NotBigoZ@users.noreply.github.com>
2020-01-11 19:11:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    
+    client.user.setActivity("TV", {type: "WATCHING"});
 });
 
 client.on('message', message => {
