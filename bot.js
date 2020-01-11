b3338acb746951a0a8c4dd71da63b1648b323287
NotBigoZ <59773657+NotBigoZ@users.noreply.github.com>
2020-01-11 19:21:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,10 +4,13 @@ const client = new Discord.Client();
 
 client.on('ready', () =>{
 console.log('This bot is online!') //Tells you if the bot goes online using the log
-client.user.setActivity('Calcio',{ //Put the text you want it to say as the activity here in the ' '
-type: 'WATCHING' //Sets activity. Uses "WATCHING" "STREAMING" "PLAYING
-}).catch(console.error); //Catches if an error happens
-
+client.user.setPresence({
+        game: {
+            name: 'Use f!help',
+            type: "Playing",
+            url: "https://discordapp.com/"
+        }
+    });
 })
 
 
