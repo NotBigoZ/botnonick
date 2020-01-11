4f19890edadbf74f41f39fa2e8f0eb540a7aa7af
NotBigoZ <59773657+NotBigoZ@users.noreply.github.com>
2020-01-11 19:58:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.user.setStatus('online')
 client.user.setPresence({
         game: {
             name: 'Use nn!help',
-            type: "Watching",
+            type: "Listening",
             url: "https://www.nonick.it/"
         }
     });
