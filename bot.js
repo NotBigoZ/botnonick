fec18f7199bc391ff6e76fd9c2f551daf1e7064b
NotBigoZ <59773657+NotBigoZ@users.noreply.github.com>
2020-01-13 15:55:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,6 +47,25 @@ client.on('message', message => {
 
 
 
+const client = require('discord-rich-presence')('180984871685062656');
+ 
+client.updatePresence({
+  state: 'slithering',
+  details: '🐍',
+  startTimestamp: Date.now(),
+  endTimestamp: Date.now() + 1337,
+  largeImageKey: 'snek_large',
+  smallImageKey: 'snek_small',
+  instance: true,
+});
+
+
+
+
+
+
+
+
 
 
 
