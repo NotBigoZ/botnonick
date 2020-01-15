8261f3c652d4d447115dc874eea16846d4b1fead
NotBigoZ <59773657+NotBigoZ@users.noreply.github.com>
2020-01-15 16:37:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-game = discord.Game("658648498530549763")
-
 
 
 client.on('ready', () =>{
-console.log('This bot is online!') //Tells you if the bot goes online using the log
-//client.user.setStatus('online')
-/*await*/ client.change_presence(status=discord.Status.online, activity=game)
-/*client.user.setPresence({
+console.log('This bot is online!')
+client.user.setPresence({
         game: {
             name: 'Use nn!help',
             type: "Listening",
-            url: "https://www.nonick.it/"
+            url: ""
         }
-    });*/
+    });
 })
 
 
